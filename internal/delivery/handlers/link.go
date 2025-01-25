fca4270165260589b074e3970246cface2c35a30
NemoFlip <artgamer3163@gmail.com>
2025-01-25 13:54:12 +0300

    made: link server logic

---
+++ internal/delivery/handlers/link.go
@@ -0,0 +1,26 @@
+package handlers
+
+import "github.com/gin-gonic/gin"
+
+type LinkServer struct {
+}
+
+func NewLinkServer() *LinkServer {
+	return &LinkServer{}
+}
+
+func (ls *LinkServer) Create(ctx *gin.Context) {
+
+}
+
+func (ls *LinkServer) GoTo(ctx *gin.Context) {
+
+}
+
+func (ls *LinkServer) Update(ctx *gin.Context) {
+
+}
+
+func (ls *LinkServer) Delete(ctx *gin.Context) {
+
+}
---
+++ internal/delivery/router/link.go
@@ -0,0 +1,16 @@
+package router
+
+import (
+	"URLProject/internal/delivery/handlers"
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterLinkRoutes(r *gin.Engine, linkServer *handlers.LinkServer) {
+	linkGroup := r.Group("/link")
+	{
+		linkGroup.POST("", linkServer.Create)
+		linkGroup.PATCH("/{id}", linkServer.Update)
+		linkGroup.DELETE("", linkServer.Delete)
+		linkGroup.GET("/{alias}", linkServer.GoTo)
+	}
+}
